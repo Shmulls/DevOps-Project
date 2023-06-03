d5ff48480461780b93ff297832e26b063822db2d
Shmuel Malikov <sshmoel1@gmail.com>
2023-06-03 17:05:10 +0300

    Test added.

--- tests/avg.spec.js
+++ tests/avg.spec.js
@@ -11,3 +11,14 @@ describe("avg", () => {
     });
   });
 });
+
+describe("negative", () => {
+  describe("#negative", () => {
+    it("calculates the average correctly with negative grades", () => {
+      const grades = [90, -80, 70, -60];
+      const sum = grades.reduce((total, grade) => total + grade, 0);
+      const average = sum / grades.length;
+      expect(average).to.equal(5);
+    });
+  });
+});
